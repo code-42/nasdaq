4fc5d37af041c27bb2c7e16416dca4dd9aed0427
Edward Dupre <edwd42@gmail.com>
2017-09-28 17:48:09 +0000

    add login controller

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,17 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-var ctrlStocks = require('../controllers/stock.controllers.js');
+var ctrlStocks = require('../controllers/stocks.controllers.js');
 var ctrlQuery = require('../controllers/querys.controllers.js');
 
 router
   .route('/')
   .get(ctrlStocks.stocksGetAll);
   // .post(ctrlStocks.queryAddOne);
+  // .get(ctrlStocks.getStockSymbol);
 
 router
   .route('/stock')
-  // .get(ctrlStocks.stocksGetAll)
+  .get(ctrlStocks.stocksGetAll)
   .post(ctrlQuery.query);
   // .post(ctrlStocks.queryAddOne);
 
